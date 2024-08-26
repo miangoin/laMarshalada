fb7816707ac67f017b82ea21333ebe337a34c7f9
Miguel Angel Gonzalez <miangoin@Air-de-Miguel.home>
2024-08-26 15:23:55 +0200

    prueba deploy

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import StatsPage from './Pages/Stats';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/laMarshalada"> 
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />        
