827c4b9bc7a9890cc8fe1029e9046ab74ebe923b
Miguel Angel Gonzalez <miangoin@Air-de-Miguel.home>
2025-02-11 07:44:11 +0100

    test

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -15,6 +15,7 @@ function Navbar() {
           <Link to="/about" className="hover:text-gray-300">Acerca de mi</Link>
           <Link to="/stats" className="hover:text-gray-300">Picks/Stats</Link>
           <Link to="/casas" className="hover:text-gray-300">Casas de Apuestas</Link>
+          <Link to="/test" className="hover:text-gray-300">Casas de Apuestas</Link>
           
         </div>
       </div>
