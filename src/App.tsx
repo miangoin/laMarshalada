429ad328259af51bd5d49f47a35b4a37be640e7f
Miguel Angel Gonzalez <miangoin@Air-de-Miguel.home>
2025-02-14 14:51:01 +0100

    prueba simple

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './Components/Navbar';
-import HomePage from './Pages/Home';
-import AboutPage from './Pages/About';
-import StatsPage from './Pages/Stats';
-import Casas from './Pages/Casas';
+// import Navbar from './Components/Navbar';
+// import HomePage from './Pages/Home';
+// import AboutPage from './Pages/About';
+// import StatsPage from './Pages/Stats';
+// import Casas from './Pages/Casas';
 import Test from './Pages/Test'
 function App() {
   return (
     <Router basename="/laMarshalada">
-      <Navbar />
+      {/* <Navbar /> */}
       <Routes>
-        <Route path="/" element={<HomePage />} />        
-        <Route path="/about" element={<AboutPage />} />
+        <Route path="/" element={<Test />} />        
+        {/* <Route path="/about" element={<AboutPage />} />
         <Route path="/stats" element={<StatsPage />} />
         <Route path="/casas" element={<Casas />} />
-        <Route path="/test" element={<Test />} />
+        <Route path="/test" element={<HomePage />} /> */}
       </Routes>
     </Router>
   );
--- src/Pages/Test.tsx
+++ src/Pages/Test.tsx
@@ -72,16 +72,6 @@ const Test: React.FC = () => {
           </div>
         </section>
 
-        {/* Excel Section */}
-        <section className="bg-gray-800 rounded-xl p-6 mb-6 min-h-[800px] relative overflow-hidden">
-          <div className="absolute inset-0 p-4">
-            <iframe
-              src="https://docs.google.com/spreadsheets/u/0/d/1x7MIthsd3Oyrt2v_iuOV7T00TZ6Wol65NAIQ-n_wSW0/edit?pli=1"
-              style={{ width: "100%", height: "100%" }}
-              className="border-none"
-            ></iframe>
-          </div>
-        </section>
 
         {/* Casino Section */}
         <div className="container mx-auto py-8 px-4">
